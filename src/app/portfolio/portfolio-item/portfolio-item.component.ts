dc0b8cda23d1c6bd407e1e2820c890cfda9c5db6
Artur Milancej <a.milancej@gmail.com>
2020-04-07 14:54:24 +0200

    added some fixes and styling to portfolio-item

--- src/app/portfolio/portfolio-item/portfolio-item.component.ts
+++ src/app/portfolio/portfolio-item/portfolio-item.component.ts
@@ -13,9 +13,10 @@ export class PortfolioItemComponent implements OnInit {
     title: 'test',
     description: `Lorem ipsum dolor sit amet,
     consectetur adipiscing elit,
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
-    image: null,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    image: '../../../assets/img/xd.jpg',
     technologies: null,
     responibilities: null,
     extraInfo: null,
